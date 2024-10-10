d61341ba3e2a11e1505a9582f70dec439e73540f
mayronmiguel <mayronmiguel@gmail.com>
2024-10-10 07:15:47 -0300

    02
    Corrigindo teste recebe idade

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -4,7 +4,7 @@ from pytest import mark
 
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
-        entrada = '13/03/2000' # Given-Contexto
+        entrada = '13/03/2002' # Given-Contexto
         esperado = 22
 
         funcionario_teste = Funcionario('Teste', entrada, 1111)
